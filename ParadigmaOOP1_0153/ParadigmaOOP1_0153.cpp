1a3cf4bfdda14f4ceaddea14d78e4bcaf4497dea
Afnan Asilah <afnanyogs@gmail.com>
2024-05-21 13:55:59 +0700

    menampilkan data mahasiswa

--- ParadigmaOOP1_0153/ParadigmaOOP1_0153.cpp
+++ ParadigmaOOP1_0153/ParadigmaOOP1_0153.cpp
@@ -11,6 +11,11 @@ public:
 	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
 
 	}
+	~mahasiswa() {
+		cout << "Id=" << id << endl;
+		cout << "Nama=" << id << endl;
+	}
+
 
 	
 
